d109d41f951a0f69284f96bfc18f032354eb2b46
sulaebha <rosesulaebha@gmail.com>
2023-07-21 12:59:21 +0530

    july21

--- D21-20230720/practice/con_list_to_str.py
+++ D21-20230720/practice/con_list_to_str.py
@@ -1,3 +1,5 @@
+import json
+
 eb_bill_details=[{'month': 1, 'unit consumed': 100, 'bill_amount': 200}, 
                  {'month': 2, 'unit consumed': 150, 'bill_amount': 300},
                  {'month': 3, 'unit consumed': 300, 'bill_amount': 1500}, 
@@ -14,14 +16,35 @@ eb_bill_details=[{'month': 1, 'unit consumed': 100, 'bill_amount': 200},
 #     # traverse in the string
 #     for ele in eb_bill_details:
 #         str1 += ele
- 
+
 #     # return string
 #     return str1
- 
+
 eb_string=str(eb_bill_details)
+
+# eb_bill_details_json=json.dumps(eb_string)
+# print(eb_bill_details_json)
+method=input("enter the covertion method:")
+
+if method=="json"and"Json"and"JSON"and"JsOn"and"JSon":
+    eb_bill_details_json=json.dumps(eb_string)
+    print(eb_bill_details_json)
+
+elif method=="dict":    
+    eb_bill_details_new=str(eb_bill_details)
+    print(eb_bill_details_new)
+
 # print(eb_string)
 # print(listToString(eb_bill_details))
 
-eb_string_open=open("/home/sulaebha/karka/D21-20230720/practice/convert_str.txt",'r')
-for line in eb_string_open:
-    print(line)
+# eb_string_open=open("/home/sulaebha/karka/D21-20230720/practice/convert_str.txt",'r')
+# for line in eb_string_open:
+#     print(line)
+
+# eb_string_open.close()
+
+# eb_string_open=open("/home/sulaebha/karka/D21-20230720/practice/convert_str.txt",'a')
+
+
+# eb_string_open.write("hiii")
+# eb_string_open.close()
---
+++ D22-20230721/practice/remove_dup.py
@@ -0,0 +1,16 @@
+dup_value_list=[1,2,3,2,5,1,5]
+original_value_list=[]
+
+
+
+dup_value_list.sort()
+# [1, 1, 2, 2, 3, 5, 5]
+# print(dup_value_list)
+
+
+for value in dup_value_list:
+#     # print(value)
+   if  value!=value+1:
+      original_value_list.append(value)
+
+   print(original_value_list)        
